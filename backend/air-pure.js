53d0bd0b82d2842f56509afd5f9e732e4549f4b4
Heitor <heitormorak@gmail.com>
2022-07-28 00:19:13 -0300

    Add backend structure
    - including middleware

---
+++ backend/air-pure.js
@@ -0,0 +1,46 @@
+import fetch from "node-fetch";
+
+// const server = 'https://jsonplaceholder.typicode.com'
+// function GetTemp(){
+//     fetch('https://jsonplaceholder.typicode.com/todos/').then((response) => {
+//         //após receber retorno da promise (response)
+//         console.log(response)
+
+//         response.json()
+//     })
+// }
+// GetTemp();
+
+const server = 'https://airpurelanding.vercel.app/'
+
+//primeiro precisa fazer login no dispositivo
+function LoginAirPure(){
+    fetch({server} /aip/login)
+        .then((response) => {
+        console.log(response.json())        
+    })
+}
+
+//obtendo infos do ambiente
+function GetInfoAmbientes(){
+    fetch({server} /api/ambientes/{id})
+        .then((response) => {
+        console.log(response.json())        
+    })
+}
+
+//consulta leituras da data x
+function GetLeiturasDia(){
+    fetch({server} /api/mediaDia/{parametro}/{idAmbiente}/{data})
+        .then((response) => {
+        console.log(response.json())        
+    })
+}
+
+//consulta leituras da data x
+function GetLeiturasDia(){
+    fetch({server} /api/mediaDia/{parametro}/{idAmbiente}/{data})
+        .then((response) => {
+        console.log(response.json())        
+    })
+}
