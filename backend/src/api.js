3e346d3ad718dc1e08eb99b288e0da02a7e28ca5
Rodrigo F. Coutinho <cvc.francisco@gmail.com>
2022-08-11 14:02:18 -0300

    adicionando comunicação com banco, model e rota de teste

--- backend/src/api.js
+++ backend/src/api.js
@@ -2,7 +2,10 @@ import express from 'express'
 import cors from 'cors'
 import { LoginAirPure, GetInfoAmbientes, GetLeiturasDia, GetUltimaLeitura  } from './air-pure.js'
 
+import { GetAmostragens } from './routes/amostragem.js'
+
 const app = express()
+const port = 8080;
 
 app.use(express.json())
 app.use(cors())
@@ -13,7 +16,10 @@ app.get('/infoAmbientes', GetInfoAmbientes)
 app.get('/leiturasDia', GetLeiturasDia)
 app.get('/ultimaLeitura', GetUltimaLeitura)
 
+//rotas NeoNatAir
+app.get('/api/amostragens', GetAmostragens);
 
-app.listen(8080,()=>{
-    console.log("listen")
+
+app.listen(port,()=>{
+    console.log(`Server is running in the port: ${port}`)
 })
