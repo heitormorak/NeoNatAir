cb6e0a26ef0b99a0dfa073efe326e885d345c85e
Heitor <dev04@nk.local>
2022-09-15 14:29:10 -0300

    ajust Login page

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
-
+import logo from "../assets/logo.png"
 
 
 const Login =() => {
@@ -33,38 +33,61 @@ const Login =() => {
         console.log("ERRO");
         }        
     }
+
+    function Register() {
+        navigate('/register');
+    }
     
  
     return (     
-        <section className="section is-centered">
-            <div  className="hero-body">
-                <div className="container">
-                    <div className="columns is-centered">
-                        <div className="column">
-                            <div className="box">
-                                <p className="has-text-centered">{msg}</p>
-                                <div className="field mt-5">
-                                    <label className="label">Email</label>
-                                    <div className="controls">
-                                        <input type="text" className="input" placeholder="Username" value={email} onChange={(e) => setEmail(e.target.value)} />
+        <section className="section is-centered" >
+            <div style={{  backgroundColor: '#add2eb',
+                            width: '100%',
+                            height: '100%',
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            padding: '80px',
+                            borderRadius: '15px' }}>
+                <div style={{ width: '50%', height: '50%' }}>
+                    <img src={logo}></img>
+                </div>
+                <div  className="hero-body">
+                    <div className="container" >
+                        <div >
+                            <div className="column" >
+                                
+                                    <p className="has-text-centered">{msg}</p>
+                                    <div className="field mt-5" style={{width: '400px'}}>
+                                        <label className="label">Email</label>
+                                        <div className="controls">
+                                            <input type="text" className="input" placeholder="Username" value={email} onChange={(e) => setEmail(e.target.value)} />
+                                        </div>
                                     </div>
-                                </div>
-                                <div className="field mt-5">
-                                    <label className="label">Password</label>
-                                    <div className="controls">
-                                        <input type="password" className="input" placeholder="******" value={password} onChange={(e) => setPassword(e.target.value)} />
+                                    <div className="field mt-5" style={{width: '400px'}}>
+                                        <label className="label">Password</label>
+                                        <div className="controls">
+                                            <input type="password" className="input" placeholder="******" value={password} onChange={(e) => setPassword(e.target.value)} />
+                                        </div>
+                                        <div style={{ display: 'flex', justifyContent:'center'}}>
+                                        <div className="field mt-5" style={{ justifyContent: 'space-between', width:'200px',  padding: '5px', display:'flex'}}>
+                                            <button onClick={() => Auth()}>Login</button>  
+                                            <button onClick={() => Register()}>Register</button>                                
+                                        </div>   
+                                        </div>
+                                                            
                                     </div>
-                                <div className="field mt-5">
-                                    <button onClick={() => Auth()}>Login</button>  
-                                    <button onClick={() => Auth()}>Register</button>                                
-                                </div>                               
-                                </div>
+                                    
+                               
+                                
                             </div>
-                            
                         </div>
                     </div>
                 </div>
+                <p style={{  textAlign: 'center' }}>v.1.0.0.1 </p>
             </div>
+            
         </section>
     )
 }
